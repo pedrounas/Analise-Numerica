c1dd1e599ab54e7a1cd55fdd8f11e03cdf44236c
Pedro Unas <shockwave2k11@gmail.com>
2019-02-25 15:00:22 +0000

    Started work on 2nd problem!

--- Trabalho-1/test.py
+++ Trabalho-1/test.py
@@ -1,6 +1,16 @@
+from __future__ import division
+import numpy as np
+import math
+
 count = 0
 
-while (count < 9):
-    print count
+def adder( n ):
+    sum = 0
+    for i in range(0, n + 1):
+        sum += math.factorial(i)**2/math.factorial(2*i + 1)
+    return sum * (9/(2*math.sqrt(3)))
+
+while (count < 10):
+    print '%.15f' % np.sum(np.absolute(adder(count)) - adder(count))
     count = count + 1
-    
+
