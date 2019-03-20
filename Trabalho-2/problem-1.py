d0e9d57bcd0bb8b82cbb3601567f3d432e40cb97
Pedro Unas <shockwave2k11@gmail.com>
2019-03-20 11:50:44 +0000

    Done 1b)

--- Trabalho-2/problem-1.py
+++ Trabalho-2/problem-1.py
@@ -4,9 +4,10 @@ def main():
     f = lambda x: x**2 - (math.cos(x))**2
     df = lambda x: 2*x + math.sin(2*x)
     eps = epsilon()
-    NewtonMethod(f,df,-1,1,eps)
+    firstZero = NewtonMethod(f,df,-1,1,eps)
+    getInterval(firstZero,10**-1)
 
-def NewtonMethod(f,df,a,b,eps,):
+def NewtonMethod(f,df,a,b,eps):
     it = 0
     x = a
     while True:
@@ -17,6 +18,12 @@ def NewtonMethod(f,df,a,b,eps,):
             break
         x = xn
     print("X Value -> %0.20f" % x, "| Iterations ->", it, "| A value ->", a, "| B value ->", b)
+    return x
+
+def getInterval(zero,amp):
+    leftBound = zero - amp
+    rightBound = zero + amp
+    print("[%0.20f" % leftBound,"; %0.20f]" % rightBound)
 
 def epsilon():
     eps = 1
