15b8fb6fc5a2709652e89b2a3ba53685d97d8f97
Pedro Unas <shockwave2k11@gmail.com>
2019-04-28 22:41:55 +0100

    Added visualization for cubic splines!

--- Trabalho-3/trabalho3.py
+++ Trabalho-3/trabalho3.py
@@ -5,10 +5,27 @@ from scipy import interpolate
 
 
 def main():
-    # showFunction()
-    startLagrange()
-    # cubicSpline()
+	#exercicio1a
 
+	#exercicio1b (Precisa do input dos pontos, comentar para testar)
+    #cubicSpline(False, None, None)
+	
+	#exercicio 2a
+	cubicSpline(True, None, None)
+	
+	#exercicio 2b I
+	f = lambda x: 4*np.power(x,2) + np.sin(9*x)
+	points = createPoints(f)
+	#exercicio 2b II
+	cubicSpline(True,points,f)
+
+def createPoints(f):
+	xPoints = np.linspace(-1,1,9)
+	yPoints = []
+	for i in xPoints:
+		yPoints.append(round(f(i),3))
+	points = zip(xPoints,yPoints)
+	return points
 
 def showFunction():
     points = np.linspace(0, 10, 100)
@@ -156,24 +173,37 @@ def printNewton(n, x0, xVal, yVal):
 
 # returns a function
 
+def cubicSpline(default, Outpoints, originalFunction):
+	points = []
 
-def cubicSpline():
-    xPoints=[]
-    yPoints=[]
-    x=typeCheck(input("Insira a abcissa pretendida: "))
-    n=int(input("Insira o valor de n: "))
-    print("Insira as n abcissas: ")
-    for i in range(1, n+2):
-        xPoints.append(input())
-    print("Insira as n ordenadas: ")
-    for i in range(1, n+2):
-        yPoints.append(input())
-
-    coefTuples=interpolate.splrep(xPoints, yPoints)
-    print(coefTuples)
-    spline=interpolate.splev(x, coefTuples)
-    print(spline)
-    return
-
+	if (default == False): #Insere pontos à mão
+		graph = plt.figure('Exercicio 1b')
+		n = int(input("Insira o número de pontos a inserir:"))
+		for i in range(1,n+1):
+			xn = float(input("Insira a abcissa " + str(i) + ":"))
+			yn = float(input("Insira a ordenada " + str(i) + ":"))
+			points.append( (xn, yn) )
+	elif (default == True and Outpoints == None): #Usa os pontos default
+		points = [ (0, 1.4) , (1, 0.6) , (2, 1.0) , (2.5, 0.6) , (3, 0.6), (4, 1.0) ]
+		graph = plt.figure('Exercicio 2a (Spline Cúbico)')
+	else: #Usa a função do 2b
+		points = Outpoints
+		graph = plt.figure('Exercicio 2b II (Spline Cúbico)')
+	x, y = zip(*points)
+	subplot = graph.add_subplot(111)
+	plt.scatter(x, y, color='black')
+	for xy in zip(x, y):
+		subplot.annotate('(%s, %s)' % xy, xy=xy, textcoords='offset points')
+	temp1,temp2 = interpolate.splprep([x, y], s=0)
+	spaceP = np.arange(0, 1.01, 0.01)
+	spline = interpolate.splev(spaceP, temp1)
+	plt.plot(x, y, 'orange', spline[0], spline[1], 'blue')
+	if (Outpoints == None):
+		plt.legend(['Pontos', 'Spline Cubico'])
+	else:
+		originalP = np.arange(-1.0,1.01,0.01)
+		plt.plot(originalP, originalFunction(originalP), 'red')
+		plt.legend(['Pontos', 'Spline Cubico', 'Função Original'])
+	plt.show()
 
 main()
